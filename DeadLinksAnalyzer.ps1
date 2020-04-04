99f3fdbe4b16ba1e089379ed3514c6e056ee77f0
Frederik Hjorslev Poulsen <frederik@hjorslev.com>
2020-04-04 22:01:33 +0200

    :sparkles: (Dead links analyzer): Add dead links analyzer

---
+++ DeadLinksAnalyzer.ps1
@@ -0,0 +1,27 @@
+﻿function Test-MarkdownLinks([String]$Path) {
+    $unreachable = @()
+    # Get markdown files recursively
+    $files = Get-ChildItem -Path $path -Recurse -Include "*.md"
+
+    $files | ForEach-Object {
+        $fileName = $_.Name
+        Write-Host "Analyzing $fileName"
+
+        $urls = Select-String -Path $_ -Pattern "\[.+\]\((http.*?)\)" | ForEach-Object { $_.matches.Groups[1] } | Select-Object
+
+        $urls | ForEach-Object {
+            $url = $_.Value
+            Write-Host "Requesting url $url"
+
+            try {
+                $request = Invoke-WebRequest -Uri $url
+            } catch {
+                Write-Error "Found dead url $url in $fileName"
+                $unreachable += $url
+            }
+        }
+    }
+    # Output urls
+    return $unreachable
+}
+$deadlinks = Test-MarkdownLinks -Path ".\readme.md"
